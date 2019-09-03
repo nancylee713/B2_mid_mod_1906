cf574fbe175534afc638000056f4207f3438c2b4
Nancy Lee <srlee713@gmail.com>
2019-09-02 19:30:49 -0600

    Add resources routes for books and authors in restful style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/books", to: "books#index"
-  get "/authors/:id", to: "authors#show"
+  root to: 'books#index'
+  resources :books, :authors
+  # get "/books", to: "books#index"
+  # get "/authors/:id", to: "authors#show"
 end
